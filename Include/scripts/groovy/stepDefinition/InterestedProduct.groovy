c27f26fb7fb7f7cdc60a13086cf06140cb88a73f
Riswanda  Al Farisi <riswanda26@gmail.com>
2022-11-01 21:02:27 +0700

    [Riswanda] Updating groovy scripts error

--- Include/scripts/groovy/stepDefinition/InterestedProduct.groovy
+++ Include/scripts/groovy/stepDefinition/InterestedProduct.groovy
@@ -1,22 +1,22 @@
 package stepDefinition
+
 import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
 
 import com.kms.katalon.core.annotation.Keyword
 import com.kms.katalon.core.checkpoint.Checkpoint
-import com.kms.katalon.core.checkpoint.CheckpointFactory
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling
 import com.kms.katalon.core.testcase.TestCase
-import com.kms.katalon.core.testcase.TestCaseFactory
 import com.kms.katalon.core.testdata.TestData
-import com.kms.katalon.core.testdata.TestDataFactory
-import com.kms.katalon.core.testobject.ObjectRepository
 import com.kms.katalon.core.testobject.TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 
 import internal.GlobalVariable
 
@@ -42,9 +42,8 @@ import cucumber.api.java.en.Given
 import cucumber.api.java.en.Then
 import cucumber.api.java.en.When
 
-
-// Riswanda
-class Search {
+//Riswanda
+class InterestedProduct {
 	@Then("User Login")
 	public void user_login() {
 		WebUI.callTestCase(findStepDefinition('FeatureLogin/Test Cases/StepDefinition/Feature Login/LOGIN001- Login With Valid Email and Password'), [:], FailureHandling.STOP_ON_FAILURE)
@@ -59,12 +58,12 @@ class Search {
 	public void user_click_hamburger_menu() {
 		WebUI.callTestCase(findTestObject('Pages/InterestedProduct/Click Hamburger Menu'),[:], FailureHandling.STOP_ON_FAILURE)
 	}
-	
+
 	@Then("User Click Interested Product")
 	public void user_click_category_interested() {
 		WebUI.callTestCase(findTestObject('Pages/InterestedProduct/Click Interested Product Category'),[:], FailureHandling.STOP_ON_FAILURE)
 	}
-	
+
 	@Then("User Verify Interested Product Category")
 	public void user_verify_interested_product_category() {
 		WebUI.verifyElementVisible(findTestObject('Pages/InterestedProduct/Verify Content Interested Product Category'),[:], FailureHandling.STOP_ON_FAILURE)
